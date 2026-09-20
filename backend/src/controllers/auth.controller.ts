88fbebd8efb9104926784527131ca5dc737a780f
Rajat pandey <shadyaf6@gmail.com>
2026-09-21 01:48:51 +0530

    update: auth.controller -> changed cookie option

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -17,7 +17,7 @@ import AppError from "../utils/appError";
 const REFRESH_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict" as const,
+  sameSite: "none" as const,
   maxAge: 30 * 24 * 60 * 60 * 1000,
 };
 
